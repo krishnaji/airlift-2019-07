92da69c2ae89472bd23b90a65b3163b171a44588
Shrikrishna Khose <krishna.khose@gmail.com>
2019-09-10 13:25:08 -0400

    Update main.tf

--- aks_advnet_rbac/main.tf
+++ aks_advnet_rbac/main.tf
@@ -31,7 +31,7 @@ resource "azurerm_policy_assignment" "test" {
   policy_definition_id = "${azurerm_policy_definition.test.id}"
   description          = "Policy Assignment created via an Acceptance Test"
   display_name         = "Location Policy Assignment"
-  # depends_on           = [azurerm_resource_group.test]
+  depends_on           = [azurerm_resource_group.test]
 
   parameters = <<PARAMETERS
   {
